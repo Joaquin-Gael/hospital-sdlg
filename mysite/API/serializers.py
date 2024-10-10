3158b2fce1bd43df878143d24ff626d41e645a1b
Just Fontaine <itsmealeexvii@gmail.com>
2024-10-09 22:43:49 -0300

    Serializers de la API

---
+++ mysite/API/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+from turnero.models import * 
+from medicos.models import (Ubicaciones,Medicos,Departamentos,Especialidades,Horario_medicos)
+from user.models import Usuarios
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuarios
+        fields = '__all__'
+        
+class UbicationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ubicaciones
+        fields = '__all__'
+        
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Departamentos
+        fields = '__all__'
+        
+class SpecialtySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Especialidades
+        fields = '__all__'
+        
+class DoctorsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Medicos
+        fields = '__all__'
+
+class SchedulesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Horario_medicos
+        fields = '__all__'
+        
+class AppointmentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Citas
+        fields = '__all__'
+
+class ShiftsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Turnos
+        fields = '__all__' 
+        
