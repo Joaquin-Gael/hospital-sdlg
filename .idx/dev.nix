129b5183d411e457ce5e82e2ce58ee5e47833032
Just Fontaine <itsmealeexvii@gmail.com>
2024-10-09 22:27:09 -0300

    Entorno con Nix

---
+++ .idx/dev.nix
@@ -0,0 +1,48 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.python3
+    pkgs.python-launcher
+    pkgs.cope
+    pkgs.dart-sass
+    pkgs.nodejs_20
+  ];
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "ms-python.python"
+    ];
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        create-venv = ''
+          python -m venv .venv
+          source .venv/bin/activate
+          pip install -r mysite/requirements.txt
+        '';
+        # Open editors for the following files by default, if they exist:
+        default.openFiles = ["README.md" "mysite/mysite/urls.py"];
+      };
+      # To run something each time the workspace is (re)started, use the `onStart` hook
+    };
+    # Enable previews and customize configuration
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["./devserver.sh"];
+          env = {
+            PORT = "$PORT";
+          };
+          manager = "web";
+        };
+      };
+    };
+  };
+}
