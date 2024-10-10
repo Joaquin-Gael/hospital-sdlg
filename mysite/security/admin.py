3d37d9f54e90aa16cfb97174473f699d207fd19e
Just Fontaine <itsmealeexvii@gmail.com>
2024-10-09 23:15:07 -0300

    ‎Modelos registrados en el administrador de Django

---
+++ mysite/security/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import BlackListTokens, SessionTokens
+
+# Register your models here.
+@admin.register(BlackListTokens)
+class BlackListTokensAdmin(admin.ModelAdmin):
+    list_display = ('tokenJTID', 'created_at', 'expires_at', 'DB')
+    list_filter = ('DB',)
+    search_fields = ('tokenJTID',)
+    ordering = ('-created_at',)
+
+@admin.register(SessionTokens)
+class SessionTokensAdmin(admin.ModelAdmin):
+    list_display = ('tokenUUID', 'created_at', 'expires_at')
+    search_fields = ('tokenUUID',)
+    ordering = ('-created_at',)
