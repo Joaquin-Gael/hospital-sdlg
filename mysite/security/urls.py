3d59d7fdaf88afb32583c48625563495d5aec2a3
Just Fontaine <itsmealeexvii@gmail.com>
2024-10-09 23:17:22 -0300

    ‎Lógica de urls

---
+++ mysite/security/urls.py
@@ -0,0 +1,66 @@
+from django.urls import path, re_path
+from django.conf import settings
+from django.views.generic.base import RedirectView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from .views import *
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Security API",
+        default_version='v1.0.5',
+        description="Descripción: SubAPI Para El Servicio De Seguridad\nAunque en el swager te diga que el obligatorio pasar el username no lo es en la API",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email=settings.EMAIL_HOST_USER),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path(
+        '',
+        RedirectView.as_view(
+            url='/API/security/swagger/',
+            permanent=False
+        )
+    ),
+    path(
+        'token/', 
+        SecurityToken.as_view(), 
+        name='token_obtain_pair'
+    ),
+    path(
+        'token/refresh/', 
+        SecurityTokenRefresh.as_view(), 
+        name='token_refresh'
+    ),
+
+    # Swagger UI
+    re_path(
+        r'^swagger(?P<format>\.json|\.yaml)$', 
+        schema_view.without_ui(
+            cache_timeout=0
+        ),
+        name='schema-json'
+    ),
+    re_path(
+        r'^swagger/$', 
+        schema_view.with_ui(
+            'swagger', 
+            cache_timeout=0
+        ),
+        name='schema-swagger-ui'
+    ),
+    # Opción de Documentación ReDoc
+    re_path(
+        r'^redoc/$', 
+        schema_view.with_ui(
+            'redoc', 
+            cache_timeout=0
+            ),
+        name='schema-redoc'
+    ),
+]
