a17a30d27b7199c773c1a143d527e9f2002b40a0
Just Fontaine <itsmealeexvii@gmail.com>
2024-10-09 22:43:06 -0300

    Lógica de views.py de la API

---
+++ mysite/API/views.py
@@ -0,0 +1,102 @@
+from rest_framework import (viewsets,status,response,decorators,permissions)
+from .serializers import *
+from turnero.models import * 
+from medicos.models import (Ubicaciones,Medicos,Departamentos,Especialidades,Horario_medicos)
+from user.models import Usuarios
+import json
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = Usuarios.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    @decorators.action(['GET'], url_path='turnos', detail=True)
+    def get_by_user(self, request, pk=1):
+        try:
+            turnos = Turnos.objects.filter(userID=pk)
+            list_turn = []
+            for x in turnos:
+                data = {
+                    'id': x.TurnoID, 
+                    'medico': {
+                        'id': x.citaID.medicoID.medicoID,
+                        'nombre': x.citaID.medicoID.nombre
+                    },
+                    'horario': {
+                        'id': x.citaID.horarioID.horarioID,
+                        'hora': str(x.citaID.horarioID.hora_inicio)
+                    },
+                    'motivo': x.motivo, 
+                    'estado': x.estado  
+                }
+                json.dumps(data)
+                list_turn.append(data)
+                
+            json.dumps(list_turn)
+            return response.Response(list_turn, status=status.HTTP_200_OK)
+        except Exception as err:
+            print(err)
+            return response.Response({
+                'err':err.__class__
+            }, status=status.HTTP_404_NOT_FOUND)
+    
+    @decorators.action(['GET'], url_path='data', detail=True)
+    def get_user_data(self, request, pk=1):
+        try:
+            user = Usuarios.objects.get(userID=pk)
+            data = {
+                'nombre':user.nombre,
+                'apellido':user.apellido,
+                'email':user.email,
+                'telefono':user.telefono,
+                'imagen':user.imagen,
+                'dni':user.dni,
+                'contrseña':user.get_contraseña()
+            }
+
+            json.dumps(data)
+            return response.Response(data, status=status.HTTP_200_OK)
+        except Exception as e:
+            print(e)
+            return response.Response({
+                'err':e.__class__
+            },status=404)
+    
+class UbicationViewSet(viewsets.ModelViewSet):
+    queryset = Ubicaciones.objects.all()
+    serializer_class = UbicationSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class DepartmentViewSet(viewsets.ModelViewSet):
+    queryset = Departamentos.objects.all()
+    serializer_class = DepartmentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class SpecialtyViewSet(viewsets.ModelViewSet):
+    queryset = Especialidades.objects.all()
+    serializer_class = SpecialtySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class DoctorsViewSet(viewsets.ModelViewSet):
+    queryset = Medicos.objects.all()
+    serializer_class = DoctorsSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class SchedulesViewSet(viewsets.ModelViewSet):
+    queryset = Horario_medicos.objects.all()
+    serializer_class = SchedulesSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class AppointmentsViewSet(viewsets.ModelViewSet):
+    queryset = Citas.objects.all()
+    serializer_class = AppointmentsSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class ShiftsViewSet(viewsets.ModelViewSet):
+    queryset = Turnos.objects.all()
+    serializer_class = ShiftsSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
